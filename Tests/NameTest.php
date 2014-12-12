43851d1814d325eac9f4c81d01eddf3b23b0cad1
George Yates III <me@georgeyatesiii.com>
2014-12-12 18:30:39 -0500

    Modernizing all the code and updating composer.json including namespaces

--- Tests/NameTest.php
+++ Tests/NameTest.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'PHPUnit/Framework.php';
-require_once dirname(__FILE__).'/../Name.php';
+require_once dirname(__FILE__) . '/../Name.php';
 
 class NameTest extends PHPUnit_Framework_TestCase {
 
